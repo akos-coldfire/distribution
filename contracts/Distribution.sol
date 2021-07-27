c0d0b7b056f61a427404090965b3b5f22a1ae46f
akos-coldfire <79063810+akos-coldfire@users.noreply.github.com>
2021-07-27 19:13:23 +0300

    Update Distribution.sol

--- contracts/Distribution.sol
+++ contracts/Distribution.sol
@@ -13,22 +13,22 @@ contract Distribution is Ownable {
     enum State{deposited, distributed, removed}
 
     struct BeneficiaryInfo {
-        address beneficiary; // Address of beneficiary
-        uint256 amountToken; // TOKENs amount
+        address beneficiary; 
+        uint256 amountToken; 
     }
 
     RewardToken public token;
+    bool private isLocked = true;
 
     mapping(address => BeneficiaryInfo) public beneficiaries;
 
     event Deposit(address indexed from, address indexed to, uint256 amount, State state);
-    event AddBeneficiary(address indexed who, uint256 amount, State state);
     event DecreaseReward(address indexed who, uint256 amount, State state);
     event EmergencyWithdraw(address indexed to, uint256 amount, State state);
 
     constructor(
         address _token
-    ) payable {
+    ) {
         require(
             _token != address(0),
              "Token Address is not Zero"
@@ -37,10 +37,6 @@ contract Distribution is Ownable {
     }
 
     modifier checkForZero(address _beneficiaries, uint256 _amount) {
-        require(
-            owner() == _msgSender(), 
-            "Ownable: caller is not the owner"
-        );
         require(
             _beneficiaries != address(0),
             "Address of Beneficiaries cannot be zero"
@@ -52,10 +48,18 @@ contract Distribution is Ownable {
         _;
     }
 
+    modifier whenNotLocked() {
+        require(
+            !isLocked,
+            "Not allowed - locked"
+        );
+        _;
+    }
+
     // Should transfer tokens from owner to distribution contract
     function deposit(
         uint256 _amount
-    ) public payable onlyOwner {
+    ) public onlyOwner {
         require(
             _amount != 0, 
             "Amount cannot be zero"
@@ -69,57 +73,52 @@ contract Distribution is Ownable {
         address[] memory _beneficiaries,
         uint256[] memory _amount
     ) public onlyOwner {
+        require(_beneficiaries.length == _amount.length, "Length of arrays is invalid");
         for(uint i; i < _beneficiaries.length; i++) {
-            // check if address isn`t empty
             require(
                 address(_beneficiaries[i]) != address(0),
                 "Address of Beneficiaries cannot be zero"
             );
         }
         for(uint i; i < _amount.length; i++) {
-            // check if amount`s isn`t null
+
             require(
                 _amount[i] != 0,
                 "Balances of Beneficiaries cannot be zero"
             );
         }
-        BeneficiaryInfo memory beneficiaryInfo;
+        BeneficiaryInfo storage beneficiaryInfo;
         for(uint i; i < _beneficiaries.length; i++) {
+            beneficiaryInfo = beneficiaries[_beneficiaries[i]];
             beneficiaryInfo.beneficiary = _beneficiaries[i];
             beneficiaryInfo.amountToken = _amount[i];
             beneficiaries[_beneficiaries[i]] = beneficiaryInfo;
-            token.safeApprove(_beneficiaries[i], _amount[i]);
-            token.safeTransferFrom(owner(), _beneficiaries[i], _amount[i]);
-            emit AddBeneficiary(_beneficiaries[i], _amount[i], State.distributed);
         }
     }
 
     // Should add beneficiary with tokens amount from owner
     function addBeneficiary(
-        address _beneficiaries,
+        address _beneficiary,
         uint256 _amount
-    ) public checkForZero(_beneficiaries, _amount) {
-        BeneficiaryInfo memory beneficiaryInfo;
-        beneficiaryInfo.beneficiary = _beneficiaries;
+    ) public checkForZero(_beneficiary, _amount) onlyOwner {
+        BeneficiaryInfo storage beneficiaryInfo = beneficiaries[_beneficiary];
+        beneficiaryInfo.beneficiary = _beneficiary;
         beneficiaryInfo.amountToken = _amount;
-        beneficiaries[_beneficiaries] = beneficiaryInfo;
-        token.safeApprove(_beneficiaries, _amount);
-        token.safeTransferFrom(owner(), _beneficiaries, _amount);
-        emit AddBeneficiary(_beneficiaries, _amount, State.distributed);
+        beneficiaries[_beneficiary] = beneficiaryInfo;
     }
 
     // Should decrease amount of rewards for benefeciary
     function decreaseReward(
         address _beneficiary, 
         uint256 _amount
-    ) public checkForZero(_beneficiary, _amount) {
-        token.safeDecreaseAllowance(_beneficiary, _amount);
+    ) public checkForZero(_beneficiary, _amount) onlyOwner {
+        beneficiaries[_beneficiary].amountToken -= _amount;
         emit DecreaseReward(_beneficiary, _amount, State.removed);
     }
     
-    // Should transfer amout of reward tokens back to the owner
+    // Should transfer amount of reward tokens back to the owner
     function emergencyWithdraw(uint256 _amount) 
-        external payable onlyOwner {
+        public onlyOwner {
         require(
             _amount != 0, 
             "Amount cannot be zero"
@@ -133,14 +132,13 @@ contract Distribution is Ownable {
     }
 
     // Should transfer reward tokens to beneficiary
-    function _claim() internal {
-        token.safeTransfer(msg.sender, token.balanceOf(this.owner()));
+    function claim() whenNotLocked public {
+        token.safeTransfer(msg.sender, beneficiaries[msg.sender].amountToken);
     }
 
     // Should lock/unlock rewards for beneficiary
-    function lockRewards(bool _lock) external onlyOwner {
-        if (_lock) _claim(); else{}
+    function lockRewards(bool _lock) public onlyOwner {
+        isLocked = _lock;
     }
 
-    //fallback() external {}
 }
